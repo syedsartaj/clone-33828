4fa2a1569fba1edb8f29cc996f34d7a9b83b8932
Syed Sartaj Ahmed <140163200+syedsartaj@users.noreply.github.com>
2025-07-08 06:47:33 +0530

    Update route.ts

--- app/post-sitemap.xml/route.ts
+++ app/post-sitemap.xml/route.ts
@@ -31,7 +31,7 @@ ${rows
     const editedTitle = title.toLowerCase().replace(/\s+/g, '-');
     return `
   <url>
-    <loc>${baseUrl}/${editedtitle}</loc>
+    <loc>${baseUrl}/${editedTitle}</loc>
     <lastmod>${new Date(robottxt_publish_date || new Date()).toISOString()}</lastmod>
   </url>`;
   })
