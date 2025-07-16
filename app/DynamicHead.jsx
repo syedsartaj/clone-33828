336303ae507d67b59437fb8cfda7062249446ad6
Syed Sartaj Ahmed <140163200+syedsartaj@users.noreply.github.com>
2025-07-16 06:16:40 +0530

    Update DynamicHead.jsx

--- app/DynamicHead.jsx
+++ app/DynamicHead.jsx
@@ -5,12 +5,12 @@ import { useContext } from 'react';
 import { DataContext } from './DataContext';
 
 export default function DynamicHead() {
-  const { title, description } = useContext(DataContext);
+  const { Heading, companySlogan } = useContext(DataContext);
 
   return (
     <Head>
-      <title>{title || 'Smaksly — Build & Deploy Your Website'}</title>
-      <meta name="description" content={description || 'Smaksly helps you create and launch modern websites effortlessly, including free hosting and SEO tools.'} />
+      <title>{Heading || 'Smaksly — Build & Deploy Your Website'}</title>
+      <meta name="description" content={companySlogan || 'Smaksly helps you create and launch modern websites effortlessly, including free hosting and SEO tools.'} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="https://i.postimg.cc/cLjLPgpD/Chat-GPT-Image-Jul-9-2025-01-34-08-AM.png" />
     </Head>
